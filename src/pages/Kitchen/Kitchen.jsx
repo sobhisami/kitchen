35178d35c7c3ee9e96328b980970ebec70ef5eee
sobhisami <sobhisami073@gmail.com>
2023-08-16 15:31:27 +0300

    kitchen page

---
+++ src/pages/Kitchen/Kitchen.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import About from '../../sections/About'
+import Blog from '../../sections/Blog'
+import Footer from '../../sections/Footer'
+import Ingredients from '../../sections/Ingredients'
+import HeroSection from '../../sections/HeroSection'
+import Menu from '../../sections/Menu'
+import Offer from '../../sections/offer'
+import Reviews from '../../sections/Reviews'
+import Services from '../../sections/Services'
+import Statistics from '../../sections/Statistics'
+
+
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <HeroSection />
+        <About />
+        <Services />
+        <Menu />
+        <Offer />
+        <Ingredients />
+        <Statistics />
+        <Reviews />
+        <Blog />
+        <Footer />
+      </div>
+    )
+  }
+}
